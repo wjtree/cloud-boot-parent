7b5c9ae0ffc2e654ec25461e1ece6d7b1f395350
wangj <wangj@13322.com>
2018-05-03 11:48:32 +0800

    初始化框架

---
+++ mould-server/src/main/java/com/app/server/MouldServerApplication.java
@@ -0,0 +1,12 @@
+package com.app.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MouldServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MouldServerApplication.class, args);
+    }
+}
---
+++ mould-web/src/main/java/com/app/web/MouldWebApplication.java
@@ -0,0 +1,12 @@
+package com.app.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MouldWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MouldWebApplication.class, args);
+    }
+}
