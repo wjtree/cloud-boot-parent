ec6c0fc94de69bed2c43768bab56b80a60a42f47
wangj <wangj@13322.com>
2018-06-08 14:20:51 +0800

    初始化 spring cloud 脚手架

--- mould-web/src/test/java/com/app/web/WebApplicationTests.java
+++ mould-web/src/test/java/com/app/web/WebApplicationTests.java
@@ -1,13 +1,48 @@
 package com.app.web;
 
+import com.app.web.consumer.HelloConsumer;
+import lombok.SneakyThrows;
+import lombok.extern.log4j.Log4j2;
+import org.apache.commons.fileupload.disk.DiskFileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.io.IOUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+@Log4j2
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class WebApplicationTests {
+    @Autowired
+    HelloConsumer helloConsumer;
+
+    @Test
+    @SneakyThrows
+    public void testUpload() {
+        File file = new File("upload.txt");
+        DiskFileItem fileItem = (DiskFileItem) new DiskFileItemFactory().createItem("file", MediaType.TEXT_PLAIN_VALUE, true, file.getName());
+
+        try (InputStream input = new FileInputStream(file); OutputStream os = fileItem.getOutputStream()) {
+            IOUtils.copy(input, os);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Invalid file: " + e, e);
+        }
+
+        MultipartFile multi = new CommonsMultipartFile(fileItem);
+
+        log.info(helloConsumer.upload(multi));
+    }
 
     @Test
     public void contextLoads() {
